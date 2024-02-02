e92ca33df09462d6a4121bfb89873537e255503e
qai33413 <qai33413@adobe.com>
2024-02-02 12:57:39 +0530

    update model

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^5afp=lk92d*hj%otb6qgh)q8tirzrg)rynv51&!s)-i)5&&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
