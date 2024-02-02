813a0ff0896cd418a9050532e8e114787b877dcd
qai33413 <qai33413@adobe.com>
2024-02-02 12:36:07 +0530

    Add requirements.txt

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=^5afp=lk92d*hj%otb6qgh)q8tirzrg)rynv51&!s)-i)5&&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
